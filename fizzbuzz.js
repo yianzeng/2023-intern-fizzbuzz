804893af621f9bd5ae57f44151db1da9d537eeb8
Yian Zeng <Yian.Zeng@softwire.com>
2023-06-27 10:50:01 +0100

    New FizzBuzz fn

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 // This is our main function
-function fizzbuzz() {
+function fizzbuzz_old() {
     for (let num = 1; num < 101; num++) {
 
         if (num % 3 == 0) {
@@ -18,7 +18,21 @@ function fizzbuzz() {
     }
 }
 
-
+function fizzbuzz() {
+    for (let num = 1; num < 16; num++) {
+        let fizz = '', buzz = '';
+        if (num % 3 === 0) {
+            fizz = 'Fizz'
+        }
+        if (num % 5 === 0) {
+            buzz = 'Buzz'
+        } if (fizz+buzz) {
+            console.log(fizz+buzz)
+        } else {
+            console.log(num)
+        }
+    }
+}
 // Now, we run the main function:
 fizzbuzz();
 
