b8c6613c2dd0db30066580a8dc96171603819daf
Yian Zeng <Yian.Zeng@softwire.com>
2023-06-27 10:21:14 +0100

    First working FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,23 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
+    for (let num = 1; num < 101; num++) {
 
-    // Put your code here...
-
+        if (num % 3 == 0) {
+            if (num % 5 == 0) {
+                console.log('FizzBuzz')
+            } else {
+                console.log('Fizz')
+            }
+        }
+        else if (num % 5 == 0) {
+            console.log('Buzz')
+        }
+        else {
+            console.log(num)
+        }
+    }
 }
+
 
 // Now, we run the main function:
 fizzbuzz();
